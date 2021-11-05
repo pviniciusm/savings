9a98b71ffae7e6f66db83679cb407ec0f2250d69
Paulo Cardoso <pvinics@gmail.com>
2021-11-05 16:07:51 -0300

    jest installation

---
+++ src/domain/usecases/createIncome.spec.ts
@@ -0,0 +1,7 @@
+import { CreateIncomeUseCase } from './createIncome';
+
+describe('create income use case tests', () => {
+    test('should return anything in order to check if jest is installed', () => {
+        expect(1).toBe(2 - 1);
+    });
+});
