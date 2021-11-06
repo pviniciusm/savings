52f9541cab2495e6d52bddefca5dcf5633af0d77
Paulo Cardoso <pvinics@gmail.com>
2021-11-06 18:09:18 -0300

    add: usage of income routes on express server init + usage of express json to handle with json body requests

--- src/server/serverInit.ts
+++ src/server/serverInit.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import incomeRoutes from './routes/income-routes';
 
 const serverInit = () => {
     const app = express();
+    app.use(express.json());
+
+    app.use('/income', incomeRoutes);
 
     return new Promise<any>((resolve, reject) => {
         app.listen(8081);
