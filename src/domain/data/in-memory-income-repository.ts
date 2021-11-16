e92ae2c1c4c522216a23165405f944e025e41e45
pviniciusm <pvinics@gmail.com>
2021-11-16 14:07:18 -0300

    refactor: set incomeList as property of InMemoryIncomeRepository

--- src/domain/data/in-memory-income-repository.ts
+++ src/domain/data/in-memory-income-repository.ts
@@ -1,20 +1,27 @@
 import { Income } from '../entities';
 import { IIncomeRepository } from '../repositories/income-repository';
 
-const incomeList: Array<Income> = [
-    new Income(5000, 'Salário', new Date('2021-11-07'), true),
-    new Income(20000, 'Growdev', new Date('2021-11-07'), true),
-    new Income(100, 'Freela #1', new Date('2021-11-07'), true),
-    new Income(500, 'Pix do cliente', new Date('2021-11-07'), true),
-    
-];
-
 export class InMemoryIncomeRepository implements IIncomeRepository {
+    public incomeList: Array<Income>;
+
+    constructor(initialIncomeList?: Array<Income>) {
+        if(initialIncomeList) {
+            this.incomeList = initialIncomeList;
+        } else {
+            this.incomeList = [
+                new Income(5000, 'Salário', new Date('2021-11-07'), true),
+                new Income(20000, 'Growdev', new Date('2021-11-07'), true),
+                new Income(100, 'Freela #1', new Date('2021-11-07'), true),
+                new Income(500, 'Pix do cliente', new Date('2021-11-07'), true),
+            ];
+        }
+    }
+
     async create(income: Income): Promise<void> {
-        incomeList.push(income);
+        this.incomeList.push(income);
     }
 
     async list(id?: string): Promise<Array<Income>> {
-        return id ? incomeList.filter(income => income.getId() == id) : [...incomeList];
+        return id ? this.incomeList.filter(income => income.getId() == id) : [...this.incomeList];
     }
 }
