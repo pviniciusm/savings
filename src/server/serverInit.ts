98192efee2927781034b74ee06118a102ce9126c
pviniciusm <pvinics@gmail.com>
2021-12-16 12:02:06 -0300

    feat: set invoice and outcome routes to express server initialization

--- src/server/serverInit.ts
+++ src/server/serverInit.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import incomeRoutes from './routes/income-routes';
+import outcomeRoutes from './routes/outcome-routes';
+import invoiceRoutes from './routes/invoice-routes';
 
 const serverInit = () => {
     const app = express();
@@ -8,6 +10,8 @@ const serverInit = () => {
     app.use(cors());
 
     app.use('/income', incomeRoutes);
+    app.use('/outcome', outcomeRoutes);
+    app.use('/invoice', invoiceRoutes);
 
     let port: number | string = process.env.PORT || 8081;
     return new Promise<any>((resolve, reject) => {
