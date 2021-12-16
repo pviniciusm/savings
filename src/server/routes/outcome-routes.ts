ced4d7ebef3bf07152419578af821715d264ed69
pviniciusm <pvinics@gmail.com>
2021-12-16 12:00:37 -0300

    refactor: change routes to use express router handler

--- src/server/routes/outcome-routes.ts
+++ src/server/routes/outcome-routes.ts
@@ -3,14 +3,19 @@ import { OutcomeController } from '@/controller/outcome-controller';
 import { CreateOutcomeUseCase } from '@/domain/usecases/create-outcome/create-outcome';
 import { createOutcomeRepository } from '@/server/helpers/repositories-factory';
 import { handleExpressRequest } from '@/server/helpers/handle-express-request';
+import { ListOutcomeUseCase } from '@/domain/usecases/list-outcome/list-outcome';
 
 const routes = Router();
 
+const repository = createOutcomeRepository();
+
 // Controller initialization
 const controller = new OutcomeController(
-    new CreateOutcomeUseCase(createOutcomeRepository()),
+    new CreateOutcomeUseCase(repository),
+    new ListOutcomeUseCase(repository)
 );
 
-routes.post('/', (req: Request, res: Response) => handleExpressRequest(req, res, controller.create));
+routes.post('/', (req: Request, res: Response) => handleExpressRequest(res, () => controller.create(req, res)));
+routes.get('/', (req: Request, res: Response) => handleExpressRequest(res, () => controller.list(req, res)));
 
 export default routes;
