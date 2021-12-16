c83f4b6749a9069b7c4210a4aaa1ec72d09b0cc5
pviniciusm <pvinics@gmail.com>
2021-12-16 10:44:29 -0300

    feat: domain error base class

--- src/domain/errors/index.ts
+++ src/domain/errors/index.ts
@@ -1,4 +1,6 @@
-export class InvalidValueError extends Error {
+import { DomainError } from './domain-error';
+
+export class InvalidValueError extends DomainError {
     public readonly value?: string;
 
     constructor(value?: string) {
@@ -7,15 +9,21 @@ export class InvalidValueError extends Error {
     }
 }
 
-export class InvalidDescriptionError extends Error {
+export class InvalidDescriptionError extends DomainError {
     constructor() {
         super('Description have less than zero and than 50 characters');
     }
 }
 
-export class InvalidDateError extends Error {
+export class InvalidDateError extends DomainError {
     constructor() {
         super('Date is invalid');
+    }
+}
+
+export class RequiredFieldsError extends DomainError {
+    constructor(fields: string) {
+        super(`${fields} are required`);
     }
 }
 
