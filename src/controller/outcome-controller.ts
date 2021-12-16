bcb784a9f49a05f10b7e5767e19190f82c57aadb
pviniciusm <pvinics@gmail.com>
2021-12-16 11:54:09 -0300

    feat: add list outcome usecase to its controller

--- src/controller/outcome-controller.ts
+++ src/controller/outcome-controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express';
 import { RequiredFieldError } from '@/controller/errors/required-field-error';
 import { CreateOutcomeUseCase } from '@/domain/usecases/create-outcome/create-outcome';
+import { ListOutcomeUseCase } from '@/domain/usecases/list-outcome/list-outcome';
 
 export class OutcomeController {
     constructor(
         private readonly createOutcomeUseCase: CreateOutcomeUseCase,
+        private readonly listOutcomeUseCase: ListOutcomeUseCase,
     ) {}
 
     async create(request: Request, response: Response): Promise<Response> {
@@ -38,6 +40,21 @@ export class OutcomeController {
             message: 'ok',
             ok: true,
             data: createdOutcome
+        });
+    }
+
+    async list(request: Request, response: Response): Promise<Response> {
+        const id = request.query.id ? request.query.id as string : undefined;
+
+        const month = request.query.month ? Number(request.query.month) : undefined;
+        const year = request.query.year ? Number(request.query.year) : undefined;
+
+        let listedOutcomes = await this.listOutcomeUseCase.execute({ id, year, month });
+
+        return response.status(200).send({
+            message: 'ok',
+            ok: true,
+            data: listedOutcomes
         });
     }
 }
