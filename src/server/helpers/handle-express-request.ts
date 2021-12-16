877dc1c1dfaa861468359f17d23bfa1049c50e7f
pviniciusm <pvinics@gmail.com>
2021-12-16 11:58:38 -0300

    feat: add express error handling based on controller or domain errors

--- src/server/helpers/handle-express-request.ts
+++ src/server/helpers/handle-express-request.ts
@@ -1,13 +1,23 @@
 import { Request, Response } from 'express';
 import { Exception, RequiredFieldError } from '@/controller/errors';
+import { DomainError } from '@/domain/errors/domain-error';
+import { ControllerError } from '@/controller/errors/controller-error';
 
-export const handleExpressRequest = async (req: Request, res: Response, method: any) => {
+export const handleExpressRequest = async (
+    res: Response,
+    method: () => Promise<any>
+) => {
     try {
         return await method();
     } catch (err) {
-        if (err instanceof RequiredFieldError) {
+        if (err instanceof DomainError) {
             return res.status(400).send(Exception(err));
         }
+
+        if (err instanceof ControllerError) {
+            return res.status(401).send(Exception(err));
+        }
+
         return res.status(500).send(Exception(err));
     }
 };
